38529a00bf78fa5df80ae87e103bdbdee84c6c57
ZipFile <zipfile.d@protonmail.com>
2023-09-11 16:35:37 +0000

    Fix abi3 wheel names

--- setup.py
+++ setup.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
+from typing import Tuple
 
 from setuptools import Extension, setup
+from wheel.bdist_wheel import bdist_wheel
+
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self) -> Tuple[str, str, str]:
+        # From https://github.com/joerick/python-abi3-package-sample/blob/main/setup.py
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            # on CPython, our wheels are abi3 and compatible back to 3.8
+            return "cp38", "abi3", plat
+
+        return python, abi, plat
+
 
 setup(
+    cmdclass={"bdist_wheel": bdist_wheel_abi3},
     ext_modules=[
         Extension(
             "getdents._getdents",
