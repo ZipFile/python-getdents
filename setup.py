6549f4363a18612b0f60a25eeab5a79c0a0fc2f3
ZipFile <zipfile.d@protonmail.com>
2024-08-15 14:14:23 +0000

    Remove workaround for building abi3 wheels

--- setup.py
+++ setup.py
@@ -1,24 +1,8 @@
 #!/usr/bin/env python
-from typing import Tuple
-
 from setuptools import Extension, setup
-from wheel.bdist_wheel import bdist_wheel
-
-
-class bdist_wheel_abi3(bdist_wheel):
-    def get_tag(self) -> Tuple[str, str, str]:
-        # From https://github.com/joerick/python-abi3-package-sample/blob/main/setup.py
-        python, abi, plat = super().get_tag()
-
-        if python.startswith("cp"):
-            # on CPython, our wheels are abi3 and compatible back to 3.8
-            return "cp38", "abi3", plat
-
-        return python, abi, plat
-
 
 setup(
-    cmdclass={"bdist_wheel": bdist_wheel_abi3},
+    options={"bdist_wheel": {"py_limited_api": "cp38"}},
     ext_modules=[
         Extension(
             "getdents._getdents",
